a86b5e9bd27d11e87a60e2f0754e2c5fabbe3fb6
AmitArie <amit.arie@gmail.com>
2017-01-04 22:29:16 +0200

    Working on login page

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
+var path = require('path');
 var db = require('./db/sportivDB.js');
-db.connect();
-
-
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
     , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 
-
 var app = express();
 
+app.use('/static', express.static('public'));
+
+
 app.get('/', function (req, res) {
-    
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/login.html'));
+});
+
 
 app.listen(port, ip);
