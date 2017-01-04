8cd737274edf0929fdbc458aac554a337cb25c0a
AmitArie <amit.arie@gmail.com>
2017-01-04 21:20:46 +0200

    Server

--- server.js
+++ server.js
@@ -2,28 +2,19 @@
 //  OpenShift sample Node application
 var express = require('express');
 var db = require('./db/sportivDB.js');
-
+db.connect();
 
 
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
     , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
+
+
 var app = express();
 
 app.get('/', function (req, res) {
-    db.connect( function(err){
-        if (err){
-            res.send(err)
-        }
-        else {
-            db.query('SELECT * from test', function(err, rows, fields) {
-                if (err) res.send(error);
-                console.log('The solution is: ', rows);
-            });
-            db.end();
-        }
-    });
+    
 });
 
 app.listen(port, ip);
