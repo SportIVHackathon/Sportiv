044c376a19700f282bd3ba3317d8cf10a5f0c597
AmitArie <amit.arie@gmail.com>
2017-01-04 20:05:41 +0200

    db

--- db/sportivDB.js
+++ db/sportivDB.js
@@ -5,11 +5,11 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     :  $OPENSHIFT_MYSQL_DB_HOST,
-    port     :  $OPENSHIFT_MYSQL_DB_PORT,
-    user     : 'adminltHQzBa',
-    password : 'DESbKr7KSa29',
-    database : 'sportiv'
+    host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+    database : process.env.OPENSHIFT_APP_NAME
 });
 
 module.exports = connection;
