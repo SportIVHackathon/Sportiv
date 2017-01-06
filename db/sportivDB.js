342285c1746210a9c51fb803d69179b2ff1f78cd
amit.arie@mail.huji.ac.il <amit.arie@mail.huji.ac.il>
2017-01-06 16:25:42 +0200

    Fixing DB Security issues

--- db/sportivDB.js
+++ db/sportivDB.js
@@ -1,15 +1,11 @@
-/**
- * Created by amitarie on 04/01/17.
- */
-
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host     : process.env.OPENSHIFT_MYSQL_DB_HOST ||  "127.0.0.1",
-    user     :  process.env.OPENSHIFT_MYSQL_DB_USERNAME ||   "adminltHQzBa",
-    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD || "DESbKr7KSa29",
-    port     : process.env.OPENSHIFT_MYSQL_DB_PORT ||  "3306",
-    database : process.env.OPENSHIFT_APP_NAME || "sportiv"
+    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+    database : process.env.OPENSHIFT_APP_NAME,
 });
 
 module.exports = connection;
